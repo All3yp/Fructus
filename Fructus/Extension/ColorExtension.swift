7e1a48315852b2540b9c3be39cd3fd3debb64ddd
All3yp <noyalle@ualberta.ca>
2021-05-22 19:21:18 -0300

    Create data model and fetch data, create extension color

---
+++ Fructus/Extension/ColorExtension.swift
@@ -0,0 +1,51 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import SwiftUI
+
+// MARK: - Asset Catalogs
+
+extension Color {
+  public static let accentColor = Color.init("AccentColor")
+  public static let colorAppleDark = Color.init("ColorAppleDark")
+  public static let colorAppleLight = Color.init("ColorAppleLight")
+  public static let colorBlueberryDark = Color.init("ColorBlueberryDark")
+  public static let colorBlueberryLight = Color.init("ColorBlueberryLight")
+  public static let colorCherryDark = Color.init("ColorCherryDark")
+  public static let colorCherryLight = Color.init("ColorCherryLight")
+  public static let colorGooseberryDark = Color.init("ColorGooseberryDark")
+  public static let colorGooseberryLight = Color.init("ColorGooseberryLight")
+  public static let colorGrapefruitDark = Color.init("ColorGrapefruitDark")
+  public static let colorGrapefruitLight = Color.init("ColorGrapefruitLight")
+  public static let colorLemonDark = Color.init("ColorLemonDark")
+  public static let colorLemonLight = Color.init("ColorLemonLight")
+  public static let colorLimeDark = Color.init("ColorLimeDark")
+  public static let colorLimeLight = Color.init("ColorLimeLight")
+  public static let colorMangoDark = Color.init("ColorMangoDark")
+  public static let colorMangoLight = Color.init("ColorMangoLight")
+  public static let colorPearDark = Color.init("ColorPearDark")
+  public static let colorPearLight = Color.init("ColorPearLight")
+  public static let colorPlumDark = Color.init("ColorPlumDark")
+  public static let colorPlumLight = Color.init("ColorPlumLight")
+  public static let colorPomegranateDark = Color.init("ColorPomegranateDark")
+  public static let colorPomegranateLight = Color.init("ColorPomegranateLight")
+  public static let colorStrawberryDark = Color.init("ColorStrawberryDark")
+  public static let colorStrawberryLight = Color.init("ColorStrawberryLight")
+  public static let colorWatermelonDark = Color.init("ColorWatermelonDark")
+  public static let colorWatermelonLight = Color.init("ColorWatermelonLight")
+//  public static let apple = ImageAsset(name: "apple")
+//  public static let blueberry = ImageAsset(name: "blueberry")
+//  public static let cherry = ImageAsset(name: "cherry")
+//  public static let gooseberry = ImageAsset(name: "gooseberry")
+//  public static let grapefruit = ImageAsset(name: "grapefruit")
+//  public static let lemon = ImageAsset(name: "lemon")
+//  public static let lime = ImageAsset(name: "lime")
+//  public static let mango = ImageAsset(name: "mango")
+//  public static let pear = ImageAsset(name: "pear")
+//  public static let plum = ImageAsset(name: "plum")
+//  public static let pomegranate = ImageAsset(name: "pomegranate")
+//  public static let strawberry = ImageAsset(name: "strawberry")
+//  public static let watermelon = ImageAsset(name: "watermelon")
+//  public static let logo = ImageAsset(name: "logo")
+
+}
